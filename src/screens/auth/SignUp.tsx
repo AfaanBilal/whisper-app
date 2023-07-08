f1cc0c9bf8f3b4f9ce928137ac8a2a1e643b7106
Afaan Bilal <afaanbilal@gmail.com>
2023-07-08 19:43:09 +0530

    ✨ Sign Up

--- src/screens/auth/SignUp.tsx
+++ src/screens/auth/SignUp.tsx
@@ -9,20 +9,60 @@
  */
 
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Button, Text, TextInput } from 'react-native-paper';
 
 import SafeScreen from '../../components/SafeScreen';
+import { SpacingH, SpacingW } from '../../utils/size';
 
-const SignUp: React.FC = () => {
+const SignIn: React.FC = () => {
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const [passwordVisible, setPasswordVisible] = React.useState(false);
+
     return (
         <SafeScreen>
-            <Text>Sign In</Text>
+            <View style={styles.container}>
+                <Text variant="displayMedium">Sign Up</Text>
+
+                <View style={styles.inputContainer}>
+                    <TextInput mode="outlined" label="Name" value={name} onChangeText={text => setName(text)} style={{ width: "100%" }} />
+                    <TextInput mode="outlined" label="Email" value={email} onChangeText={text => setEmail(text)} style={{ width: "100%" }} />
+                    <TextInput mode="outlined" label="Password" value={password} onChangeText={text => setPassword(text)} style={{ width: "100%" }}
+                        secureTextEntry={!passwordVisible} right={<TextInput.Icon onPress={() => setPasswordVisible(!passwordVisible)} icon={passwordVisible ? "eye-off" : "eye"} />} />
+                </View>
+
+                <View style={styles.buttonContainer}>
+                    <Button mode="text" compact onPress={() => console.log('Pressed')}>Have an account? Sign In</Button>
+                </View>
+
+                <Button mode="contained" uppercase onPress={() => console.log('Pressed')}>Sign Up</Button>
+            </View>
         </SafeScreen>
     );
 };
 
-export default SignUp;
+export default SignIn;
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        paddingHorizontal: SpacingW.s1,
+    },
+    inputContainer: {
+        width: "100%",
+        marginTop: SpacingH.s2,
+        paddingHorizontal: SpacingW.s5,
+        paddingVertical: SpacingH.s1,
+    },
+    buttonContainer: {
+        width: "100%",
+        paddingHorizontal: SpacingW.s3,
+        flexDirection: "row",
+        justifyContent: "flex-end",
+        marginBottom: SpacingH.s1,
+    },
 });
