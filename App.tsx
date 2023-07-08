e0cf310e32346c7a740cb13df735e4da6c9bcef8
Afaan Bilal <afaanbilal@gmail.com>
2023-07-08 18:14:20 +0530

    ✨ Setup RN Paper

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
+/**
+ * Whisper
+ *
+ * A micro-blogging platform.
+ *
+ * @author    Afaan Bilal
+ * @copyright 2023 Afaan Bilal
+ * @link      https://eonyx.io
+ */
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
-            <StatusBar style="auto" />
-        </View>
+        <PaperProvider>
+            <View style={styles.container}>
+                <Text>Open up App.tsx to start working on your app!</Text>
+                <StatusBar style="auto" />
+            </View>
+        </PaperProvider>
     );
 }
 
