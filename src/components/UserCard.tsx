1bee8d110656ced4de1cbde837a7dc766d4c140e
Afaan Bilal <afaanbilal@gmail.com>
2023-07-08 22:38:00 +0530

    ✨ User card

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -12,12 +12,17 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Avatar, Text } from 'react-native-paper';
 import { Colors } from '../utils/colors';
+import { SpacingH, SpacingW } from '../utils/size';
+import { Fonts } from '../utils/fonts';
 
-const UserCard: React.FC<{ name: string }> = ({ name }) => {
+const UserCard: React.FC<{ name: string, username: string }> = ({ name, username }) => {
     return (
         <View style={styles.container}>
-            <Avatar.Image size={84} source={{ uri: "https://afaan.dev/assets/Afaan.png" }} />
-            <Text>{name}</Text>
+            <Avatar.Image size={48} source={{ uri: "https://afaan.dev/assets/Afaan.png" }} />
+            <View>
+                <Text variant="titleLarge" style={styles.name}>{name}</Text>
+                <Text variant="titleMedium" style={styles.username}>@{username}</Text>
+            </View>
         </View>
     );
 };
@@ -27,5 +32,17 @@ export default UserCard;
 const styles = StyleSheet.create({
     container: {
         flexDirection: "row",
-    }
+        alignItems: "center",
+        paddingHorizontal: SpacingW.s2,
+        paddingVertical: SpacingH.s1,
+        borderBottomColor: Colors.BLUE,
+        borderBottomWidth: 0.2,
+        gap: SpacingW.s2,
+    },
+    name: {
+        fontFamily: Fonts.SourceSansPro,
+    },
+    username: {
+        fontFamily: Fonts.SourceSansProBold,
+    },
 });
--- src/screens/Users.tsx
+++ src/screens/Users.tsx
@@ -18,50 +18,62 @@ const TEST_DATA = [
     {
         uuid: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
         name: "Afaan Bilal",
+        username: "afaanbilal",
     },
     {
         uuid: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
         name: "Afaan Bilal",
+        username: "afaanbilal",
     },
     {
         uuid: "58694a0f-3da1-471f-bd96-145571e29d72",
         name: "Afaan Bilal",
+        username: "afaanbilal",
     },
     {
         uuid: "bd7acbea-c1b1-46c2-aed5-3ad53abb28b1",
         name: "Afaan Bilal",
+        username: "afaanbilal",
     },
     {
         uuid: "3ac68afc-c605-48d3-a4f8-fbd91aa97f62",
         name: "Afaan Bilal",
+        username: "afaanbilal",
     },
     {
         uuid: "58694a0f-3da1-471f-bd96-145571e29d73",
         name: "Afaan Bilal",
+        username: "afaanbilal",
     },
     {
         uuid: "bd7acbea-c1b1-46c2-aed5-3ad53abb28b4",
         name: "Afaan Bilal",
+        username: "afaanbilal",
     },
     {
         uuid: "3ac68afc-c605-48d3-a4f8-fbd91aa97f65",
         name: "Afaan Bilal",
+        username: "afaanbilal",
     },
     {
         uuid: "58694a0f-3da1-471f-bd96-145571e29d76",
         name: "Afaan Bilal",
+        username: "afaanbilal",
     },
     {
         uuid: "bd7acbea-c1b1-46c2-aed5-3ad53abb28b7",
         name: "Afaan Bilal",
+        username: "afaanbilal",
     },
     {
         uuid: "3ac68afc-c605-48d3-a4f8-fbd91aa97f68",
         name: "Afaan Bilal",
+        username: "afaanbilal",
     },
     {
         uuid: "58694a0f-3da1-471f-bd96-145571e29d79",
         name: "Afaan Bilal",
+        username: "afaanbilal",
     },
 ];
 
@@ -70,7 +82,7 @@ const Users: React.FC = () => {
         <FlatList
             contentContainerStyle={styles.container}
             data={TEST_DATA}
-            renderItem={({ item }) => <UserCard name={item.name} />}
+            renderItem={({ item }) => <UserCard {...item} />}
             keyExtractor={item => item.uuid}
         />
     );
