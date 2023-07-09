96340e647bdda31d0a158259b2c1726d1beee0c9
Afaan Bilal <afaanbilal@gmail.com>
2023-07-09 08:55:56 +0530

    ✨ Explore: searchbar

--- src/screens/Explore.tsx
+++ src/screens/Explore.tsx
@@ -10,15 +10,20 @@
 
 import React from 'react';
 import { StyleSheet } from 'react-native';
+import { Searchbar } from 'react-native-paper';
 
 import SafeScreen from '../components/SafeScreen';
 import ScreenTitle from '../components/ScreenTitle';
 import Posts from './Posts';
+import { Colors } from '../utils/colors';
 
 const Explore: React.FC = () => {
+    const [search, setSearch] = React.useState('');
+
     return (
         <SafeScreen>
             <ScreenTitle title="Explore" />
+            <Searchbar theme={{ colors: { elevation: { level3: Colors.DARK }, outline: Colors.BLUE } }} mode="view" placeholder="Search" onChangeText={q => setSearch(q)} value={search} />
             <Posts />
         </SafeScreen>
     );
