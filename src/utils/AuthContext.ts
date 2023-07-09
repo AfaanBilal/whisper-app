49f2517c7e04042cf5b4ca73c6d70f5fb102866a
Afaan Bilal <afaanbilal@gmail.com>
2023-07-09 12:04:45 +0530

    🐛 Load token

--- src/utils/AuthContext.ts
+++ src/utils/AuthContext.ts
@@ -10,11 +10,17 @@
 
 import React from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { setAuthToken } from '../api';
 
 const STORAGE_KEY = 'accessToken';
 
 export const AuthContext = React.createContext<{ accessToken: string | null, setAccessToken: (token: string | null) => void }>({ accessToken: null, setAccessToken: () => {} });
 
-export const loadAccessToken = async () => await AsyncStorage.getItem(STORAGE_KEY);
+export const loadAccessToken = async () => {
+    const token = await AsyncStorage.getItem(STORAGE_KEY);
+    setAuthToken(token);
+    return token;
+};
+
 export const saveAccessToken = async (token: string) => await AsyncStorage.setItem(STORAGE_KEY, token);
 export const removeAccessToken = async () => await AsyncStorage.removeItem(STORAGE_KEY);
