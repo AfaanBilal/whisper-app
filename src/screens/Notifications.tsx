14c80dbcf0297877eaf6d89bd3f792434697689f
Afaan Bilal <afaanbilal@gmail.com>
2023-07-10 09:12:19 +0530

    ✨ Empty notifications

--- src/screens/Notifications.tsx
+++ src/screens/Notifications.tsx
@@ -9,9 +9,10 @@
  */
 
 import React from 'react';
-import { FlatList, RefreshControl, StyleSheet } from 'react-native';
-import { ActivityIndicator } from 'react-native-paper';
+import { FlatList, RefreshControl, StyleSheet, View } from 'react-native';
+import { ActivityIndicator, Text } from 'react-native-paper';
 import { useQuery } from '@tanstack/react-query';
+import { Entypo } from '@expo/vector-icons';
 
 import SafeScreen from '../components/SafeScreen';
 import ScreenTitle from '../components/ScreenTitle';
@@ -19,6 +20,7 @@ import { Colors } from '../utils/colors';
 import NotificationCard from '../components/NotificationCard';
 import { Profile } from '../api';
 import { SpacingH } from '../utils/size';
+import { Fonts } from '../utils/fonts';
 
 const Notifications: React.FC = () => {
     const { isLoading, isFetching, refetch, data } = useQuery({ queryKey: ['notifications'], queryFn: Profile.getNotifications });
@@ -29,23 +31,40 @@ const Notifications: React.FC = () => {
 
             {isLoading ?
                 <ActivityIndicator animating={true} size="large" color={Colors.SOFT_WHITE} style={{ paddingVertical: SpacingH.s2 }} /> :
-                <FlatList
+                (data.notifications && <FlatList
                     refreshControl={<RefreshControl colors={[Colors.SOFT_WHITE]} tintColor={Colors.SOFT_WHITE} refreshing={isFetching} onRefresh={() => refetch()} />}
                     style={styles.container}
                     contentContainerStyle={styles.container}
                     data={data.notifications}
                     renderItem={({ item }) => <NotificationCard {...item} />}
                     keyExtractor={item => item.id}
-                />
+                />)
             }
-                </SafeScreen>
+
+            {!isLoading && !data.notifications &&
+                <View style={styles.empty}>
+                    <Entypo name="bell" size={64} color={Colors.RED} />
+                    <Text variant="titleMedium" style={styles.emptyText}>No notifications yet</Text>
+                </View>
+            }
+        </SafeScreen>
     );
 };
 
-            export default Notifications;
+export default Notifications;
 
-            const styles = StyleSheet.create({
-                container: {
-                backgroundColor: Colors.DARK,
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: Colors.DARK,
+    },
+    empty: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        gap: SpacingH.s2,
+    },
+    emptyText: {
+        fontFamily: Fonts.SourceSansProSemiBold,
+        color: Colors.DARK_GRAY,
     },
 });
